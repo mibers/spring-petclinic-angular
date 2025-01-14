0f1fae7616a29372c80b866db404982744d80c0f
Martin Ibersperger <martin.ibersperger@gmx.at>
2025-01-14 09:38:33 +0100

    [PLAY-0003] Get trace from tests; add 2 more tests, some parametrization.

--- playwright/src/test/java/org/martinsoft/petclinic/owners/AddOwnerTest.java
+++ playwright/src/test/java/org/martinsoft/petclinic/owners/AddOwnerTest.java
@@ -3,10 +3,9 @@ package org.martinsoft.petclinic.owners;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Paths;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -24,13 +23,21 @@ public class AddOwnerTest {
   @BeforeEach
   void setup() {
     playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1));
+    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(0));
     context = browser.newContext();
     page = context.newPage();
+    context.tracing().start(new Tracing.StartOptions()
+      .setScreenshots(true)
+      .setSnapshots(true)
+      .setSources(true));
   }
 
   @AfterEach
-  void teardown() {
+  void teardown(TestInfo testInfo) {
+    if (context != null) {
+      String traceFileName = testInfo.getDisplayName().replace(" ", "_") + "-trace.zip";
+      context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get(traceFileName)));
+    }
     if (page != null) {
       page.close();
     }
@@ -49,13 +56,13 @@ public class AddOwnerTest {
     page.navigate("http://localhost:4200/petclinic/welcome");
   }
 
-  public void addOwner() {
+  public void addOwner(String firstName, String lastName) {
     page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Owners")).click();
     page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add New")).click();
     page.getByLabel("First Name").click();
-    page.getByLabel("First Name").fill("Max");
+    page.getByLabel("First Name").fill(firstName);
     page.getByLabel("Last Name").click();
-    page.getByLabel("Last Name").fill("Hofer");
+    page.getByLabel("Last Name").fill(lastName);
     page.getByLabel("Address").click();
     page.getByLabel("Address").fill("Rennweg 1");
     page.getByLabel("City").click();
@@ -63,7 +70,7 @@ public class AddOwnerTest {
     page.getByLabel("Telephone").click();
     page.getByLabel("Telephone").fill("2343555");
     page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add Owner")).click();
-    page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Max Hofer")).click();
+    page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(firstName + " " + lastName)).click();
   }
 
   public void listOwner() {
@@ -77,8 +84,28 @@ public class AddOwnerTest {
   @Issue("Link to Xray here...")
   void addNewOwner() {
     navigateToPage();
-    addOwner();
+    addOwner("Max", "Hofer");
     assertThat(page.getByText("Max Hofer")).isVisible();
+  }
+
+  @Test
+  @DisplayName("Add new owner 1")
+  @Description("Add new owner 1")
+  @Issue("Link to Xray here...")
+  void addNewOwner1() {
+    navigateToPage();
+    addOwner("Moritz", "Pichler");
+    assertThat(page.getByText("Moritz Pichler")).isVisible();
+  }
+
+  @Test
+  @DisplayName("Add new owner 2")
+  @Description("Add new owner 2")
+  @Issue("Link to Xray here...")
+  void addNewOwner2() {
+    navigateToPage();
+    addOwner("Sigmund", "Freund");
+    assertThat(page.getByText("Sigmund Freund")).isVisible();
   }
 
 }
